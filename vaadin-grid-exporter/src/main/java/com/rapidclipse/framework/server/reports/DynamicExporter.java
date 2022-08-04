dad1a3b24d71340a043e59cbb4417591e3704a4b
AB <a.bierler@xdev-software.de>
2022-08-04 16:59:47 +0200

    First working demo

---
+++ vaadin-grid-exporter/src/main/java/com/rapidclipse/framework/server/reports/DynamicExporter.java
@@ -0,0 +1,33 @@
+package com.rapidclipse.framework.server.reports;
+
+/*-
+ * #%L
+ * vaadin-grid-exporter
+ * %%
+ * Copyright (C) 2022 XDEV Software
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.OutputStream;
+
+import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
+import net.sf.dynamicreports.report.exception.DRException;
+
+
+@FunctionalInterface
+public interface DynamicExporter
+{
+	void export(JasperReportBuilder builder, OutputStream stream) throws DRException;
+}
