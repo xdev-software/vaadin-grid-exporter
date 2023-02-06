f85dba8d09c0fa74bd22afb00c28de1dbe5d590d
AB <a.bierler@xdev-software.de>
2023-02-06 09:51:57 +0100

    Move intro correct directory

--- vaadin-grid-exporter/src/main/java/software/xdev/vaadin/grid_exporter/wizard/GridExporterWizardState.java
+++ vaadin-grid-exporter/src/main/java/software/xdev/vaadin/grid_exporter/wizard/GridExporterWizardState.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package software.xdev.vaadin.grid_exporter.components.wizard;
+package software.xdev.vaadin.grid_exporter.wizard;
 
 import java.util.ArrayList;
 import java.util.List;
